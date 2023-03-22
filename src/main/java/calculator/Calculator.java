a5fc9433cc84c06fcccf00e6bceca2d0631e00fa
anoushkachouksey <anoushkachouksey@gmail.com>
2023-03-22 14:49:16 +0530

    volume2

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -73,31 +73,31 @@ public class Calculator {
 
 
     public double factorial(double number1) {
-        logger.info("[FACTORIAL] - " + number1);
+        logger.info("[FACTORIAL]-" + number1);
         double result = fact(number1);
-        logger.info("[RESULT - FACTORIAL] - " + result);
+        logger.info("[RESULT-FACTORIAL]-" + result);
         return result;
     }
 
 
 
     public double squareRoot(double number1) {
-        logger.info("[SQ ROOT] - " + number1);
+        logger.info("[SQ ROOT]-" + number1);
         double result = Math.sqrt(number1);
-        logger.info("[RESULT - SQ ROOT] - " + result);
+        logger.info("[RESULT-SQ ROOT]-" + result);
         return result;
     }
 
 
     public double power(double number1, double number2) {
-        logger.info("[POWER - " + number1 + " RAISED TO] " + number2);
+        logger.info("[POWER-" + number1 + " RAISED TO] " + number2);
         double result = Math.pow(number1,number2);
-        logger.info("[RESULT - POWER] - " + result);
+        logger.info("[RESULT-POWER]-" + result);
         return result;
     }
 
     public double naturalLog(double number1) {
-        logger.info("[NATURAL LOG] - " + number1);
+        logger.info("[NATURAL LOG]-" + number1);
         double result = 0;
         try {
 
@@ -109,9 +109,9 @@ public class Calculator {
                 result = Math.log(number1);
             }
         } catch (ArithmeticException error) {
-            System.out.println("[EXCEPTION - LOG] - Cannot find log of negative numbers " + error.getLocalizedMessage());
+            System.out.println("[EXCEPTION-LOG]-Cannot find log of negative numbers " + error.getLocalizedMessage());
         }
-        logger.info("[RESULT - NATURAL LOG] - " + result);
+        logger.info("[RESULT-NATURAL LOG]-" + result);
         return result;
     }
     public double fact(double num) {
